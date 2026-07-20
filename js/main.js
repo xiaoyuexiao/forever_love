2899a9ee265ddbf9fbca6cf53ac66db75f8f1814
黎肖 <794594866@qq.com>
2026-07-20 17:37:00 +0800

    fix: 光标和残影改为星星形状

--- js/main.js
+++ js/main.js
@@ -445,7 +445,7 @@ function animateNumber(el, target) {
 
 // ========== 鼠标残影效果 ==========
 function setupCursorTrail() {
-  const heartSVG = `<svg xmlns='http://www.w3.org/2000/svg' width='SIZE' height='SIZE' viewBox='0 0 24 24'><path d='M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z' fill='%23FFB6C1' fill-opacity='OPACITY' stroke='%23FF69B4' stroke-opacity='OPACITY' stroke-width='1'/></svg>`;
+  const heartSVG = `<svg xmlns='http://www.w3.org/2000/svg' width='SIZE' height='SIZE' viewBox='0 0 24 24'><path d='M12 2l2.4 7.4H22l-6.2 4.5 2.4 7.4L12 16.8l-6.2 4.5 2.4-7.4L2 9.4h7.6z' fill='%23FFD700' fill-opacity='OPACITY' stroke='%23DAA520' stroke-opacity='OPACITY' stroke-width='0.8'/></svg>`;
   const trailCount = 8;
   const trails = [];
 
